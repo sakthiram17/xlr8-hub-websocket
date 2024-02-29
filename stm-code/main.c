13e21fa81f666bf3d9bb90980209c382daddf173
Sakthi Ram <72368668+sakthiram17@users.noreply.github.com>
2024-02-29 12:10:27 +0530

    Create main.c

---
+++ stm-code/main.c
@@ -0,0 +1,612 @@
+/* USER CODE BEGIN Header */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "stdlib.h"
+#include "stm32f4xx_hal.h"
+#include <string.h>
+#include <stdio.h>
+#define FIXED_POINT_SCALE 1000
+#define SOFT_START_STOP_DELTA 0.1
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+ADC_HandleTypeDef hadc1;
+
+TIM_HandleTypeDef htim1;
+TIM_HandleTypeDef htim2;
+UART_HandleTypeDef huart6;
+UART_HandleTypeDef huart2;
+
+/* USER CODE BEGIN PV */
+
+float ref_voltage  = 400;
+float over_voltage = 420;
+int raw = 0;
+float mf = (160)*(3.34/ 4096);
+float duty = 51.2;
+int CCR_VAL = 960;
+float voltage = 0;
+float alpha = 1;
+volatile uint8_t rxDataAvailable = 0;
+float delta = 0;
+struct SensorData {
+    float voltage;
+    float current;
+    float duty_ratio;
+};
+
+struct Parameters {
+    float ref_voltage;
+    float power_limit;
+    float duty_ratio;
+    float kp;
+    float mode;
+};
+
+struct EntireData {
+    float voltage;
+    float current;
+    float set_duty;
+    float ref_voltage;
+    float duty_ratio;
+    float mode;
+    float kp;
+    float power_limit;
+};
+struct Parameters parameters;
+struct SensorData sensorData;
+struct EntireData entireData;
+
+
+/* USER CODE END PV */
+char receivedBuffer[81];
+float generateRandomVoltage(float baseVoltage, float percentageRange) {
+    float randomFactor = ((float)rand() / RAND_MAX) * 2 * percentageRange - percentageRange;
+    return baseVoltage + baseVoltage * randomFactor;
+}
+void parseStringToParameters(const char *buffer) {
+
+}
+void initializeStructures() {
+    // Initialize SensorData structure
+    struct SensorData sensorDataInit = {0.0, 0.0, 0.0};
+    sensorData = sensorDataInit;
+
+    // Initialize Parameters structure
+    struct Parameters parametersInit = {0, 0, 0, 0, 0};
+    parameters = parametersInit;
+
+    // Initialize EntireData structure
+    struct EntireData entireDataInit = {0, 0, 0, 0, 0, 0, 0};
+    entireData = entireDataInit;
+}
+
+char rBuffer[150];
+//MODE = 0 , OPEN LOOP, MODE = 1 CLOSED LOOP, MODE == 2 Soft stop
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_ADC1_Init(void);
+static void MX_TIM1_Init(void);
+static void MX_TIM2_Init(void);
+static void MX_USART2_UART_Init(void);
+static void MX_USART6_UART_Init(void);
+/* USER CODE BEGIN PFP */
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+	uint8_t MSG[35] = {'\0'};
+	parameters.ref_voltage =0.0;
+
+	 initializeStructures();
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_ADC1_Init();
+  MX_TIM1_Init();
+  MX_USART2_UART_Init();
+  MX_USART6_UART_Init();
+  void transmit_data(float voltage ,float current,float duty)
+  {
+      entireData.voltage = voltage;
+      entireData.current =(voltage*0.938);
+      entireData.duty_ratio = (duty);
+      entireData.mode = (parameters.mode);//make this short
+      entireData.ref_voltage = (parameters.ref_voltage);
+      entireData.set_duty = (parameters.duty_ratio);
+      entireData.kp = (parameters.kp);// make this short
+      entireData.power_limit = 123;
+      HAL_UART_Transmit(&huart2, ( uint8_t *)&entireData, sizeof(entireData),HAL_MAX_DELAY);
+
+
+  }
+  void receive_data()
+  {
+	  HAL_UART_Receive(&huart2, (uint8_t*)rBuffer, sizeof(rBuffer), 100);
+	  memcpy(&parameters,rBuffer,sizeof(parameters));
+
+  }
+  float read_Voltage()
+  {
+      int sum =0;
+
+      for (int i = 0; i < 5000; i++)
+      {
+          HAL_ADC_Start(&hadc1);
+          HAL_ADC_PollForConversion(&hadc1, HAL_MAX_DELAY);
+          raw = HAL_ADC_GetValue(&hadc1);
+
+           sum += raw;
+      }
+
+
+
+      int temp = sum /5000;  // Adjust the divisor accordingly
+      voltage = mf * temp;
+      return voltage;
+  }
+  /* USER CODE BEGIN 2 */
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+    /* USER CODE END WHILE */
+  HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
+  HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_2);
+  HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_3);
+
+     while (1)
+     {
+    	 receive_data();
+    	 HAL_Delay(5);
+         float x = rand() % (410 - 340 + 1) + 340;
+         duty = duty/100;
+         x = read_Voltage();
+         duty = duty*100;
+         float voltage = x;
+         ref_voltage = parameters.ref_voltage;
+         alpha=parameters.kp;
+         if(parameters.mode==0)
+         {
+        	 duty = parameters.duty_ratio;
+//        	 if(voltage>=over_voltage)
+//        	 {
+//        		 parameters.mode = 5;
+//        		 float temp = parameters.duty_ratio - 20;
+//        		 parameters.duty_ratio = temp>0?temp:0;
+//        	 }
+        	 transmit_data(x,x,duty);
+         }
+         else if(parameters.mode==1)
+         {
+
+
+                 if((voltage> (ref_voltage-delta)   &&  voltage < (ref_voltage +delta)))
+                 {
+
+                 }
+                 else if((voltage<(ref_voltage)))
+       	  	  {
+               	  if(((ref_voltage- voltage)/(100) < 0.001))
+               	  	  				 {
+               	  	  			 duty = duty + 0.01;
+               	  	  				 }
+               	  else {duty = duty + ((ref_voltage- voltage)/(100))* alpha ;
+               	  }
+
+
+       	  	  }
+       	  	  else if((voltage>(ref_voltage)))
+       	  	  {
+
+       	  		 if(((voltage- ref_voltage)/(100) < 0.001))
+       	  			  				 {
+       	  			  			 duty = duty - 0.01;
+       	  			  				 }
+       	  		 else{duty = duty - ((voltage- ref_voltage)/(100))* alpha ;}
+
+       	  	  }
+                 if(duty >= 58)
+                 {
+               	  duty = 58;
+                 }
+                 if(duty<=30)
+                 {
+               	  duty = 30;
+                 }
+
+                 TIM1->CCR1 = (int)(duty * 2000/100);
+                 TIM1->CCR2 = (int)((duty) * 2000/100);
+                 TIM1->CCR3 = (int)((duty) * 2000/100);
+
+                 sprintf(MSG, "duty ratio X = %d \r\n", (int)(duty*20));
+             transmit_data(x,x,duty);
+         }
+         else if(parameters.mode==2)
+         {
+        	 while(duty>0)
+        	 {
+        		 HAL_Delay(10);
+        		 duty =  duty - SOFT_START_STOP_DELTA;;
+        		 TIM1->CCR1=(int)(duty*20);
+        		 duty = duty/100;
+        		 transmit_data(x,x,duty);
+        		 if(duty<0)
+        		 {
+        			 duty = 0;
+        		 }
+
+        	 }
+
+         }
+         else if(parameters.mode==3)
+         {
+        	 while(duty<parameters.duty_ratio)
+        	 {
+
+        		 HAL_Delay(10);
+        		 duty = duty + SOFT_START_STOP_DELTA;
+        		 TIM1->CCR1=(int)(duty*20);
+        		 duty = duty/100;
+
+        		         duty = duty*100;
+
+        	 }
+              parameters.mode=0;
+              transmit_data(x,x,duty);
+
+         }
+
+             TIM1->CCR1 = (int)(duty * 2000/100);
+             TIM1->CCR2 = (int)((duty) * 2000/100);
+
+             TIM1->CCR3 = (int)(duty * 2000/100);
+
+
+             transmit_data(x,x,duty);
+       /* USER CODE END WHILE */
+
+       /* USER CODE BEGIN 3 */
+     }
+    /* USER CODE BEGIN 3 */
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 100;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief ADC1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_ADC1_Init(void)
+{
+
+  /* USER CODE BEGIN ADC1_Init 0 */
+
+  /* USER CODE END ADC1_Init 0 */
+
+  ADC_ChannelConfTypeDef sConfig = {0};
+
+  /* USER CODE BEGIN ADC1_Init 1 */
+
+  /* USER CODE END ADC1_Init 1 */
+
+  /** Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion)
+  */
+  hadc1.Instance = ADC1;
+  hadc1.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV4;
+  hadc1.Init.Resolution = ADC_RESOLUTION_12B;
+  hadc1.Init.ScanConvMode = DISABLE;
+  hadc1.Init.ContinuousConvMode = DISABLE;
+  hadc1.Init.DiscontinuousConvMode = DISABLE;
+  hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+  hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+  hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+  hadc1.Init.NbrOfConversion = 4;
+  hadc1.Init.DMAContinuousRequests = DISABLE;
+  hadc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+  if (HAL_ADC_Init(&hadc1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.
+  */
+  sConfig.Channel = ADC_CHANNEL_0;
+  sConfig.Rank = 1;
+  sConfig.SamplingTime = ADC_SAMPLETIME_3CYCLES;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN ADC1_Init 2 */
+
+  /* USER CODE END ADC1_Init 2 */
+
+}
+
+/**
+  * @brief TIM1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM1_Init(void)
+{
+
+  /* USER CODE BEGIN TIM1_Init 0 */
+
+  /* USER CODE END TIM1_Init 0 */
+
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+  TIM_OC_InitTypeDef sConfigOC = {0};
+  TIM_BreakDeadTimeConfigTypeDef sBreakDeadTimeConfig = {0};
+
+  /* USER CODE BEGIN TIM1_Init 1 */
+
+  /* USER CODE END TIM1_Init 1 */
+  htim1.Instance = TIM1;
+  htim1.Init.Prescaler = 0;
+  htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim1.Init.Period = 2000;
+  htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim1.Init.RepetitionCounter = 0;
+  htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_PWM_Init(&htim1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim1, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigOC.OCMode = TIM_OCMODE_PWM1;
+  sConfigOC.Pulse = 1000;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC.OCNPolarity = TIM_OCNPOLARITY_HIGH;
+  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+  sConfigOC.OCIdleState = TIM_OCIDLESTATE_RESET;
+  sConfigOC.OCNIdleState = TIM_OCNIDLESTATE_RESET;
+  if (HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  sConfigOC.Pulse = 1000;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  if (HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  if (HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sBreakDeadTimeConfig.OffStateRunMode = TIM_OSSR_DISABLE;
+  sBreakDeadTimeConfig.OffStateIDLEMode = TIM_OSSI_DISABLE;
+  sBreakDeadTimeConfig.LockLevel = TIM_LOCKLEVEL_OFF;
+  sBreakDeadTimeConfig.DeadTime = 0;
+  sBreakDeadTimeConfig.BreakState = TIM_BREAK_DISABLE;
+  sBreakDeadTimeConfig.BreakPolarity = TIM_BREAKPOLARITY_HIGH;
+  sBreakDeadTimeConfig.AutomaticOutput = TIM_AUTOMATICOUTPUT_DISABLE;
+  if (HAL_TIMEx_ConfigBreakDeadTime(&htim1, &sBreakDeadTimeConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM1_Init 2 */
+
+  /* USER CODE END TIM1_Init 2 */
+  HAL_TIM_MspPostInit(&htim1);
+
+}
+
+/**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART2_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART2_Init 0 */
+
+  /* USER CODE END USART2_Init 0 */
+
+  /* USER CODE BEGIN USART2_Init 1 */
+
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 1152000;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART2_Init 2 */
+
+  /* USER CODE END USART2_Init 2 */
+
+}
+
+/**
+  * @brief USART6 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART6_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART6_Init 0 */
+
+  /* USER CODE END USART6_Init 0 */
+
+  /* USER CODE BEGIN USART6_Init 1 */
+
+  /* USER CODE END USART6_Init 1 */
+  huart6.Instance = USART6;
+  huart6.Init.BaudRate = 115200;
+  huart6.Init.WordLength = UART_WORDLENGTH_8B;
+  huart6.Init.StopBits = UART_STOPBITS_1;
+  huart6.Init.Parity = UART_PARITY_NONE;
+  huart6.Init.Mode = UART_MODE_TX_RX;
+  huart6.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart6.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart6) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART6_Init 2 */
+
+  /* USER CODE END USART6_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+/* USER CODE BEGIN MX_GPIO_Init_1 */
+/* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+
+/* USER CODE BEGIN MX_GPIO_Init_2 */
+/* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
